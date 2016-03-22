7a7de6e01579bdfc540ef63f671027e9a37b2928
jakubknejzlik <jakub.knejzlik@gmail.com>
2016-03-22 07:26:11 +0100

    Initial Commit

---
+++ ui-tests-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ui-tests-example
+//
+//  Created by Jakub Knejzlik on 22/03/16.
+//  Copyright © 2016 Funtasty Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
